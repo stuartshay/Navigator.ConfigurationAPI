39cd52be1693748124fe63b2ea2281f0d0370595
Stuart Shay <sshay@yahoo.com>
2020-03-06 23:33:23 -0500

    Update Docker

--- src/Navigator.ConfigurationAPI/Startup.cs
+++ src/Navigator.ConfigurationAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Navigator.ConfigurationAPI.Configuration;
 
 namespace Navigator.ConfigurationAPI
@@ -26,6 +27,11 @@ namespace Navigator.ConfigurationAPI
             services.AddSingleton(Configuration);
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Navigator.ConfigurationAPI", Version = "v1" });
+            });
+
         }
 
 
@@ -35,6 +41,14 @@ namespace Navigator.ConfigurationAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Navigator.ConfigurationAPI");
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseHttpsRedirection();
 
